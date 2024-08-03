f2faca27f2c25a5186041c1d29685484aadd0350
Shubham mishra <121163825+subhammishrasun@users.noreply.github.com>
2024-08-03 12:33:34 +0530

    task.js

--- task.js
+++ task.js
@@ -1,25 +1,14 @@
-// models/Task.js
-
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    title: {
+    name: {
         type: String,
-        required: true
+        required: true,
     },
-    description: {
-        type: String,
-        required: true
+    completed: {
+        type: Boolean,
+        default: false,
     },
-    status: {
-        type: String,
-        enum: ['pending', 'in_progress', 'completed'],
-        default: 'pending'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
